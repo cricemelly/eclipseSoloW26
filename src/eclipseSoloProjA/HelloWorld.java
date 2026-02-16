8e9697af46bb9c72a360665e743478d7484f5955
Charmel Rice <crice30@emich.edu>
2026-02-16 07:07:44 -0500

    Programmer 1 edits a print statement

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello there, Github Project!");
 		System.out.println("This is step 4, add another print ln..");
-		System.out.println("Programmer 2 did this!");
+		System.out.println("Programmer 2 did this! Programmer 1 did THIS!!");
 	}
 
 }
