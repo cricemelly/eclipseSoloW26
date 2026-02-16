b8a3cb86c8df3f5b019323792c7a8f7e7ac75700
Charmel Rice <crice30@emich.edu>
2026-02-16 06:37:21 -0500

    Second Commit, Step 4a

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello there, Github Project!");
+		System.out.println("This is step 4, add another print ln..");
 	}
 
 }
